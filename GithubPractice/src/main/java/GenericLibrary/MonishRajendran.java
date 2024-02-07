f92238afb1c535a4fb5ed27ffb3694ab56a2b6dc
Monish Rajendran <Monish Rajendran@TYSS-Monish-Rajendran>
2024-02-07 17:15:52 +0530

    First Commit

---
+++ GithubPractice/src/main/java/GenericLibrary/MonishRajendran.java
@@ -0,0 +1,23 @@
+package GenericLibrary;
+
+public class MonishRajendran 
+{
+	String name = "MonishRajendran";
+	int id = 001;
+	String companyName = "Abc";
+	
+	public void eating()
+	{
+		System.out.println("Eating");
+	}
+	
+	public void sleeping()
+	{
+		System.out.println("Sleeping");
+	}
+	
+	public void watchingSeries()
+	{
+		System.out.println("Watching Series");
+	}
+}
